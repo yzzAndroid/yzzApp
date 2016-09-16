4a9134a58b802ffbaeab77ea351d24bd854f8ebb
yzz <1316234380@qq.com>
2016-09-16 19:27:49 +0800

    图标

--- app/src/main/java/com/qianfeng/yyz/shoppingproject/fragment/NavigationFragment.java
+++ app/src/main/java/com/qianfeng/yyz/shoppingproject/fragment/NavigationFragment.java
@@ -12,12 +12,15 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.qianfeng.yyz.shoppingproject.R;
 import com.qianfeng.yyz.shoppingproject.callback.UpdateContentCallback;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -47,16 +50,25 @@ public class NavigationFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_navigation, container, false);
 
         listView = (ListView) view.findViewById(R.id.lv_me);
-        List list = new ArrayList();
-        list.add("首页");
-        list.add("我的收藏");
-        list.add("我的购物车");
-        list.add("我的信息");
+        List<Map<String,Object>> list = new ArrayList();
+
+        String name[] = new String[]{"首页","我的收藏","我的购物车","我的信息"};
+        int p[]= new int[]{R.mipmap.guide_home_nm,R.mipmap.guide_tfaccount_nm,R.mipmap.guide_cart_nm,R.mipmap.guide_account_nm};
+        for (int i = 0;i<name.length;i++){
+            Map<String,Object> map = new HashMap<>();
+            map.put("text",name[i]);
+            map.put("img",p[i]);
+            list.add(map);
+        }
+
+
 
         DisplayMetrics metrics = new DisplayMetrics();
         getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
         view.setMinimumWidth(metrics.widthPixels*(2/3));
-        listView.setAdapter(new ArrayAdapter<>(getActivity(),R.layout.me_textview,list));
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(),list,R.layout.me_textview,new String[]{"text","img"},new int[]{R.id.na_tv,R.id.na_img});
+        listView.setAdapter(simpleAdapter);
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
