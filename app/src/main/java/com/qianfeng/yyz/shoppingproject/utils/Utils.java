0cab2b74b1bb3b26e6648f715b18850d1b2ed2e8
yzz <1316234380@qq.com>
2016-09-16 13:44:44 +0800

    添加了沉浸式

--- app/src/main/java/com/qianfeng/yyz/shoppingproject/utils/Utils.java
+++ app/src/main/java/com/qianfeng/yyz/shoppingproject/utils/Utils.java
@@ -1,18 +1,23 @@
 package com.qianfeng.yyz.shoppingproject.utils;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.qianfeng.yyz.shoppingproject.R;
+
 /**
  * Created by Administrator on 2016/9/16 0016.
  */
 public class Utils {
 
-    public static void addViewToEmptyList(ListView listView, Context context,String msg){
+    public static void addViewToEmptyList(ListView listView, Context context, String msg) {
         TextView textView = new TextView(context);
         ViewGroup viewGroup = (ViewGroup) listView.getParent();
         viewGroup.addView(textView);
@@ -21,5 +26,12 @@ public class Utils {
         textView.setGravity(Gravity.CENTER);
         textView.setTextSize(20);
         listView.setEmptyView(textView);
+    }
+
+    public static void setBarColor(AppCompatActivity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            activity.getWindow().setNavigationBarColor(activity.getResources().getColor(R.color.Them));
+            activity.getWindow().setStatusBarColor(activity.getResources().getColor(R.color.Them));
+        }
     }
 }
